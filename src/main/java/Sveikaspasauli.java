858b40a5be0127b93b90c9f80be68201e2a60778
User <User@DESKTOP-PL0JJAN>
2018-11-02 20:57:16 +0200

    Pirma saugyklos versija

--- src/main/java/Sveikaspasauli.java
+++ src/main/java/Sveikaspasauli.java
@@ -1,7 +1,35 @@
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Sveikaspasauli {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Spark.port(80);
+        Spark.get("/home", new Route() {
 
+            @Override
+
+            public Object handle (Request request, Response response)throws Exception {
+
+                URI path = Sveikaspasauli.class.getClassLoader().getResource("home.html").toURI();
+                String html = new String(Files.readAllBytes(Paths.get(path)), Charset.forName("UTF-8"));
+                return html;
+            }
+        });
+        Spark.post("/post", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String param = request.queryParams("vardas");
+                return "Hello: " + param;
+            }
+        });
     }
 }
